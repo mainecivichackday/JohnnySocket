352740661f9bd6a74a90aa77dba02c3211d1686f
Garrett Wilkin <garrett.wilkin@gmail.com>
2013-06-02 09:31:44 -0400

    Working control for hoisting crane.

--- cargoPort.js
+++ cargoPort.js
@@ -3,16 +3,24 @@ var board  = new five.Board();
 var util = require('util'),
     net = require('net');
 // Global variables for active digital pins.
-var pins   = [9, 10, 11],
-    servos = [];
+var BOOM = 13,
+    HOIST = 11,
+    SHIP = 12,
+    TRUCK = 10,
+    MAGNET = 9;
+var SERVOS   = [BOOM, HOIST, SHIP, TRUCK];
+    jServos = [];
 
 // Initialize array of Servo objects.
 function setServos() {
-  for (i in pins) {
-    var pin = pins[i];
+  for (i in SERVOS) {
+    var pin = SERVOS[i];
     var servo = new five.Servo(pin);
     servo.name = pin;
-    servos.push(servo);
+    servo.on("move",function(err,degrees) {
+      console.log('>>>> Servo '+servo.name+' at '+degrees);
+    });
+    jServos.push(servo);
     console.log('created Servo on pin '+pin);
   };
 }
@@ -20,9 +28,9 @@ function setServos() {
 // Find Servo given a pin
 function getServo(name) {
   var found = '';
-  for (i in servos) {
-    if (servos[i].name == name) {
-      found = servos[i];
+  for (i in jServos) {
+    if (jServos[i].name == name) {
+      found = jServos[i];
     }
   }
   return found;
@@ -47,8 +55,8 @@ board.on("ready", function() {
 
   // Make TCP connection as client.
   var client = net.connect(
-   // {host:'awesomesauce.me',port:'8130'},
-    {host:'localhost',port:'8130'},
+    {host:'awesomesauce.me',port:'7124'},
+   // {host:'localhost',port:'8130'},
     function() {//'connect' listener
       console.log('TCP client connected');
       client.write(JSON.stringify(boardDeets));
@@ -59,21 +67,37 @@ board.on("ready", function() {
 
   // Process data from TCP Server.
   client.on('data', function(data) {
-    var data = JSON.parse(data.toString());
-    console.log('Received TCP data: '+util.inspect(data));
-
-    if (data.component == 'servo') {
-      var servo = getServo(data.pin);
-      if (data.pos === 'min') {
-        console.log('moving '+servo.name+' to min.');
-        servo.max();
-      } else if (data.pos === 'max') {
-        console.log('moving '+servo.name+' to max.');
-        servo.min();
+    try {
+      var udkCommand = data.toString();
+      console.log('udkCommand: '+udkCommand);
+      if (udkCommand == 'LEFT') {
+        console.log('moving to Left.');
+      } else if (udkCommand == 'RIGHT') {
+        console.log('moving to Right.');
+      } else if (udkCommand == 'UP') {
+        var hoist = getServo(HOIST);
+        hoist.max();
+        console.log('moving upward.');
+      } else if (udkCommand == 'DOWN') {
+        var hoist = getServo(HOIST);
+        hoist.min();
+        console.log('moving downward.');
       } else {
-        console.log('moving '+servo.name+' to '+ data.pos+'.');
-        servo.move(data.pos);
+        var data = JSON.parse(data.toString());
+        console.log('Received TCP JSON data: '+util.inspect(data));
+        if (data.component == 'servo') {
+          var servo = getServo(data.pin);
+          if (data.pos === 'min') {
+            servo.max();
+          } else if (data.pos === 'max') {
+            servo.min();
+          } else {
+            servo.move(data.pos);
+          }
+        }
       }
+    } catch (err) {
+        console.log('Cannot process garbage.');
     }
 
    // client.end();
